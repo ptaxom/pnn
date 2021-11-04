cb536b58d36c85f6dcb6f2c236f893f8dc97e116
Pavel Khursau <pkhursov@biquants.com>
2021-11-04 19:55:07 +0100

    Added shape struct

---
+++ src/nn/shape.rs
@@ -0,0 +1,256 @@
+
+use std::{
+    error::Error,
+    fmt,
+    self
+};
+
+#[derive(Debug)]
+pub struct ShapeError {
+    description: String,
+}
+
+impl fmt::Display for ShapeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.description)
+    }
+}
+
+impl Error for ShapeError {}
+
+// Representing shape of layer
+#[derive(Debug)]
+pub struct Shape {
+    // Storing shape of layer in format NCHW, where
+    // N - batch size,
+    // C - channels or filters(features) size
+    // H - height
+    // W - width
+
+    // packed into vec
+    dims: Vec<usize>
+}
+
+impl Shape {
+    fn new(dims: Vec<usize>) -> Shape{
+        assert!(dims.len() > 1 && dims.len() < 5);
+        Shape {dims}
+    }
+
+    fn from_nc(n: usize, c: usize) -> Self {
+        Shape{dims: vec![n, c]}
+    }
+
+    fn from_nch(n: usize, c: usize, h: usize) -> Self {
+        Shape{dims: vec![n, c, h]}
+    }
+
+    fn from_nchw(n: usize, c: usize, h: usize, w: usize) -> Self {
+        Shape{dims: vec![n, c, h, w]}
+    }
+
+    fn N(&self) -> usize {
+        *(self.dims.get(0).unwrap()) // ?????????
+    }
+
+    fn C(&self) -> usize {
+        *(self.dims.get(1).unwrap()) // ?????????
+    }
+
+    fn H(&self) -> Option<usize> {
+        match self.dims.get(2) {
+            Some(d) => Some(*d),
+            None => None
+        }
+    }
+
+    fn W(&self) -> Option<usize> {
+        match self.dims.get(3) {
+            Some(d) => Some(*d),
+            None => None
+        }
+    }
+
+    fn concat(&self, other: &Shape, axis: usize) -> Result<Shape, ShapeError> {
+        if axis == 0 {
+            return Err(ShapeError{ description: String::from("Couldnt concat shapes by batch size")})
+        }
+        if self.dims.len() != other.dims.len(){
+            return Err(ShapeError{ description: String::from("Couldnt concat shapes with different size")})
+        }
+        let mut new_dims = vec![self.dims[0]];
+        for i in 1..self.dims.len() {
+            let dim;
+            if i == axis {
+                dim = self.dims[i] + other.dims[i]
+            } else if self.dims[i] == other.dims[i] {
+                dim = self.dims[i]
+            } else {
+                return  Err(ShapeError{ description: format!("Couldnt concat across axis {}", i)})
+            }
+            new_dims.push(dim);
+        }
+        Ok(Shape::new(new_dims))
+    }
+}
+
+
+impl PartialEq for Shape {
+    fn eq(&self, other: &Self) -> bool {
+        self.dims == other.dims
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn shape_created() {
+        let shape = Shape::new(vec![1, 2, 3]);
+        assert_eq!(shape.dims, [1, 2, 3]);
+    }
+
+    #[test]
+    fn shape_created2() {
+        let shape = Shape::new(vec![1, 2, 3]);
+        assert_ne!(shape.dims, [1, 2, 4]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn shape_ncreated() {
+        let shape = Shape::new(vec![1]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn shape_ncreated2() {
+        let shape = Shape::new(vec![1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn shapes_eq() {
+        let shape1 = Shape::from_nch(5, 6, 7);
+        let shape2 = Shape::new(vec![5, 6, 7]);
+        assert_eq!(shape1, shape2);
+    }
+
+    #[test]
+    fn shapes_eq2() {
+        let shape1 = Shape::from_nc(5, 11);
+        let shape2 = Shape::new(vec![5, 11]);
+        assert_eq!(shape1, shape2);
+    }
+
+    #[test]
+    fn shapes_neq() {
+        let shape1 = Shape::from_nch(5, 8, 7);
+        let shape2 = Shape::new(vec![5, 6, 7]);
+        assert_ne!(shape1, shape2);
+    }
+
+    #[test]
+    fn shapes_neq2() {
+        let shape1 = Shape::from_nch(5, 8, 7);
+        let shape2 = Shape::new(vec![5, 6, 7, 8]);
+        assert_ne!(shape1, shape2);
+    }
+
+    #[test]
+    fn get_n() {
+        let shape = Shape::from_nch(5, 8, 7);
+        assert_eq!(shape.N(), 5)
+    }
+
+    #[test]
+    fn get_n_fail() {
+        let shape = Shape::from_nc(6, 8);
+        assert_ne!(shape.N(), 5)
+    }
+
+    #[test]
+    fn get_c() {
+        let shape = Shape::from_nch(5, 8, 7);
+        assert_eq!(shape.C(), 8)
+    }
+
+    #[test]
+    fn get_c_fail() {
+        let shape = Shape::from_nc(6, 8);
+        assert_ne!(shape.C(), 7)
+    }
+
+    #[test]
+    fn get_h() {
+        let shape = Shape::from_nch(5, 8, 7);
+        assert_eq!(shape.H(), Some(7));
+    }
+
+    #[test]
+    fn get_h_fail() {
+        let shape = Shape::from_nc(6, 8);
+        assert_eq!(shape.H(), None)
+    }
+
+    #[test]
+    fn get_w() {
+        let shape = Shape::from_nchw(5, 8, 7, 9);
+        assert_eq!(shape.W(), Some(9))
+    }
+
+    #[test]
+    fn get_w_fail() {
+        let shape = Shape::from_nch(6, 8, 2);
+        assert_eq!(shape.W(), None)
+    }
+
+    #[test]
+    #[should_panic(expected = "Couldnt concat shapes with different size")]
+    fn concat_mismatch() {
+        let shape1 = Shape::from_nch(5, 8, 7);
+        let shape2 = Shape::new(vec![5, 8, 7, 8]);
+        shape1.concat(&shape2, 2).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "Couldnt concat shapes by batch size")]
+    fn concat_batchsize() {
+        let shape1 = Shape::from_nch(5, 8, 7);
+        let shape2 = Shape::new(vec![5, 6, 7, 8]);
+        shape1.concat(&shape2, 0).unwrap();
+    }
+
+    #[test]
+    fn concat_correct() {
+        let shape1 = Shape::from_nchw(32, 128, 64, 64);
+        let shape2 = Shape::new(vec![32, 32, 64, 64]);
+        let target = Shape::new(vec![32, 160, 64, 64]);
+        assert_eq!(shape1.concat(&shape2, 1).unwrap(), target);
+    }
+
+    #[test]
+    #[should_panic(expected = "Couldnt concat across axis 1")]
+    fn concat_mismatch_axis1() {
+        let shape1 = Shape::from_nchw(32, 128, 64, 64);
+        let shape2 = Shape::new(vec![32, 32, 64, 64]);
+        shape1.concat(&shape2, 2).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "Couldnt concat across axis 2")]
+    fn concat_mismatch_axis2() {
+        let shape1 = Shape::from_nchw(32, 32, 64, 64);
+        let shape2 = Shape::new(vec![32, 33, 65, 64]);
+        shape1.concat(&shape2, 1).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "Couldnt concat across axis 3")]
+    fn concat_mismatch_axis3() {
+        let shape1 = Shape::from_nchw(32, 32, 64, 65);
+        let shape2 = Shape::new(vec![32, 33, 64, 64]);
+        shape1.concat(&shape2, 1).unwrap();
+    }
+}
